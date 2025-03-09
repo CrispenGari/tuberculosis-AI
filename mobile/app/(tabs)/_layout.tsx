0912dbeab4dfdb1ec5bcc396a016c7960f9a7359
CrispenGari <crispengari@gmail.com>
2025-03-09 10:46:45 +0200

    tabs done

--- mobile/app/(tabs)/_layout.tsx
+++ mobile/app/(tabs)/_layout.tsx
@@ -3,34 +3,41 @@ import { Tabs } from "expo-router";
 import { useMediaQuery } from "@/hooks";
 import { Platform, StyleSheet } from "react-native";
 import { COLORS, FONTS } from "@/constants";
-import { Ionicons } from "@expo/vector-icons";
+import { Ionicons, MaterialIcons } from "@expo/vector-icons";
 import { StatusBar } from "expo-status-bar";
+import { useSettingsStore } from "@/store/useSettingsStore";
 
 const Layout = () => {
   const {
     dimension: { width },
   } = useMediaQuery();
+  const { settings } = useSettingsStore();
   return (
     <>
-      <StatusBar style="light" backgroundColor={COLORS.secondary} />
+      <StatusBar
+        style={settings.theme === "dark" ? "light" : "dark"}
+        backgroundColor={settings.theme === "dark" ? COLORS.dark : COLORS.light}
+      />
       <Tabs
         initialRouteName="index"
         screenOptions={{
           tabBarStyle: {
             height:
-              width >= 600 ? 70 : Platform.select({ ios: 100, android: 80 }), //os === "ios" ? 100 : 80,
-            backgroundColor: COLORS.transparent,
+              width >= 600 ? 70 : Platform.select({ ios: 100, android: 80 }),
+            backgroundColor:
+              settings.theme === "dark" ? COLORS.dark : COLORS.light,
             position: "absolute",
             elevation: 0,
           },
           tabBarHideOnKeyboard: true,
-          tabBarActiveTintColor: COLORS.main,
-          tabBarInactiveTintColor: COLORS.gray,
+          tabBarActiveTintColor: COLORS.secondary,
+          tabBarInactiveTintColor:
+            settings.theme === "dark" ? COLORS.gray : COLORS.main,
           headerShown: true,
           tabBarLabelStyle: {
             fontFamily: FONTS.bold,
             fontSize: 12,
-            marginTop: width >= 600 ? 10 : -10,
+            marginTop: width >= 600 ? 10 : -5,
             marginBottom: 10,
           },
         }}
@@ -38,11 +45,10 @@ const Layout = () => {
         <Tabs.Screen
           name="index"
           options={{
-            title: "Explore",
+            title: "Diagnose",
             tabBarIcon: ({ color, size }) => (
-              <Ionicons name="search-outline" color={color} size={size} />
+              <Ionicons name="analytics-outline" color={color} size={size} />
             ),
-            // header: (props) => <HomeHeader {...props} />,
           }}
         />
         <Tabs.Screen
@@ -50,7 +56,7 @@ const Layout = () => {
           options={{
             title: "History",
             tabBarIcon: ({ color, size }) => (
-              <Ionicons name="heart-outline" color={color} size={size} />
+              <MaterialIcons name="history" color={color} size={size} />
             ),
           }}
         />
@@ -60,7 +66,7 @@ const Layout = () => {
             headerShown: false,
             title: "Settings",
             tabBarIcon: ({ color, size }) => (
-              <Ionicons name="cloud-upload-outline" color={color} size={size} />
+              <Ionicons name="settings-outline" color={color} size={size} />
             ),
           }}
         />
